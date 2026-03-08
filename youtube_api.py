a1a2cf8d99ff273fd88b31a6ed83607d321624a3
ARSHIT RAJ <rajarshit53@gmail.com>
2026-03-09 02:04:26 +0530

    Update youtube_api.py

--- youtube_api.py
+++ youtube_api.py
@@ -1,15 +1,14 @@
 import requests
 
-# Aapki API Key yahan set hai
-API_KEY = "AIzaSyB1kbXAnaSOc_Oxu6n7DJD-jwMABDqGMtk"
+[span_2](start_span)API_KEY = "AIzaSyB1kbXAnaSOc_Oxu6n7DJD-jwMABDqGMtk"[span_2](end_span)
 
 def get_subscribers(channel_id):
-    url = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={API_KEY}"
+    [span_3](start_span)url = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={API_KEY}"[span_3](end_span)
     try:
         response = requests.get(url)
         data = response.json()
         if "items" in data and len(data["items"]) > 0:
-            return data["items"][0]["statistics"]["subscriberCount"]
+            [span_4](start_span)return data["items"][0]["statistics"]["subscriberCount"][span_4](end_span)
         return "0"
     except Exception:
         return "Updating..."
